93066c5d7d464d6957c978e8e6fecc0a5c50d5ef
ghauzar <ghauzarakbar02@gmail.com>
2021-10-25 18:49:35 +0700

    first commit

---
+++ bin/studi_kasus_1.dart
@@ -0,0 +1,14 @@
+void main(List<String> arguments) {
+  //Variables
+  String nama   = "Ghauzar Andhika Akbar";
+  String kelas  = "3A";
+  String alamat = "Puhpelem, Wonogiri"; 
+  int nim       = 20533243;
+  String NIM    = nim.toString();  
+
+  //Output
+  print("Nama   \t: $nama");
+  print("NIM    \t: $NIM");
+  print("Kelas  \t: $kelas");
+  print("Alamat \t: $alamat");
+}
